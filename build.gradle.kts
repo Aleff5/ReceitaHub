a54e06e35de29d6fa859f7057acc1f1aa25e8f85
aleff campos <aleff.mc@sempreceub.com>
2025-06-24 08:29:03 -0300

    fix: gradle n  libs version

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,10 @@
-// Top-level build file where you can add configuration options common to all sub-projects/modules.
+// In C:\Users\aleff\AndroidStudioProjects\ReceitaHub\build.gradle.kts
+
+// Make sure the version is NOT hardcoded here.
+// Instead, use the alias and set 'apply false'.
 plugins {
+    // This line applies the plugin to sub-projects, referencing the version from libs.versions.toml
     alias(libs.plugins.android.application) apply false
+    // You might have other plugins here, like for Kotlin
+    // alias(libs.plugins.kotlin.android) apply false
 }
